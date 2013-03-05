8cc0942fe66159ce7e953b81cd0aa725d1a9360e
Manuel Schulze <manuel_schulze@i-entwicklung.de>
2013-03-05 07:59:24 +0100

    * Schnittstellen spezifiziert um Enums im Frontend in Drop-Down-Listen verwenden zu können

---
+++ src/main/java/de/iew/framework/domain/utils/I18nNamedEnum.java
@@ -0,0 +1,21 @@
+package de.iew.framework.domain.utils;
+
+/**
+ * Specifies an interface to declare an Enum as "named". This means the Enum has a name an can be used as a value list
+ * in the frontend.
+ * <p>
+ * This interface should be used to resolve the Enum name through a message bundle.
+ * </p>
+ *
+ * @author Manuel Schulze <mschulze@geneon.de>
+ * @since 23.02.13 - 23:24
+ */
+public interface I18nNamedEnum {
+
+    /**
+     * Gets the name message key.
+     *
+     * @return the name message key
+     */
+    public String getNameKey();
+}
---
+++ src/main/java/de/iew/framework/domain/utils/NamedEnum.java
@@ -0,0 +1,13 @@
+package de.iew.framework.domain.utils;
+
+/**
+ * Specifies an interface to declare an Enum as "named". This means the Enum has a name an can be used as a value list
+ * in the frontend.
+ *
+ * @author Manuel Schulze <mschulze@geneon.de>
+ * @since 23.02.13 - 23:28
+ */
+public interface NamedEnum {
+
+    public String getName();
+}
